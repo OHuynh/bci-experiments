fix cov computation for each trial

src/core/data.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class Data:
     def __init__(self, eeg, nb_trials, frequency, y_dec, one_hot, y_txt, map_label, chan):
-        self._eeg = eeg
+        self._eeg = eeg #[step, trial, electrode]
         self._nb_trials = nb_trials
         self._frequency = frequency
         self._y_dec = y_dec
@@ -19,4 +19,9 @@ class Data:
         self._eeg, self._chan = filter_fn(self._eeg, self._chan)
 
     def compute_features(self):
-        self._features = np.cov(self._eeg.T.reshape(len(self._chan), -1))
+        for trial in self._nb_trials:
+            self._features.append(np.cov(self._eeg[:, trial, :].T.reshape(len(self._chan), -1)).flatten())
+
+    @property
+    def features(self):
+        return self._features